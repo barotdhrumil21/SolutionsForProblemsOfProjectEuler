bfc38e1f4d48ce5183f293dcac67652797987d2e
barotdhrumil21 <barotdhrumil21@gmail.com>
2017-02-13 18:42:11 +0530

    All prime number till one million ar in list now(Error)
    All prime numbers are supposed to be in lists by now but it goves ArrayIndexOutOfBoundException and I don' know why..

--- project_euler_solns/src/Problem_50.java
+++ project_euler_solns/src/Problem_50.java
@@ -1,7 +1,12 @@
+import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Problem_50 {
+    public static int ifNumberIsAddedOnce=0;
     public static void main(String[] args){
         List<Integer> listOfTwoDigitLongNums=new ArrayList<Integer>();
         List<Integer> listOfThreeAndFourDigitLongNums=new ArrayList<Integer>();
@@ -12,28 +17,160 @@ public class Problem_50 {
         listOfTwoDigitLongNums.add(2,5);
         listOfTwoDigitLongNums.add(3,7);
         int init=10;
-        for(int i=0;i<1_000_000;i++){
+        for(int i=0;i<1000000;i++){
             int length=(int)Math.log10(init) + 1;
-            switch (init){
+            switch (init) {
                 case 1:
-                length=2;
-                    for (int j = 0; j <init/3 ; j++) {
-                        if(init%listOfTwoDigitLongNums.get(j)==0){
+                    length = 2;
+
+                    for (int j = 0; j < init / 3; j++) {
+                        if (init % listOfTwoDigitLongNums.get(j) == 0) {
+                            ifNumberIsAddedOnce = 0;
+                            j = init / 3;
+                            try {
+                                listOfTwoDigitLongNums.remove(listOfTwoDigitLongNums.indexOf(init));
+                            } catch (Exception e) {
+
+                            }
+                        } else if (ifNumberIsAddedOnce != 1) {
                             listOfTwoDigitLongNums.add(init);
+                            ifNumberIsAddedOnce = 1;
                         }
-                        
+
+
                     }
+                    init = init + 1;
+                    ifNumberIsAddedOnce = 0;
                     break;
                 case 2:
-                    if(length==3 || length==4){
-                        for (int j = 0; j < init/10; j++) {
-                            if(init%)
+
+                    length = 3;
+                {
+                    for (int j = 0; j < init / 10; j++) {
+                        if (init % listOfThreeAndFourDigitLongNums.get(j) == 0) {
+                            ifNumberIsAddedOnce = 0;
+                            j = init / 10;
+                            try {
+                                listOfThreeAndFourDigitLongNums.remove(listOfThreeAndFourDigitLongNums.indexOf(init));
+                            } catch (Exception e) {
+
+                            }
+                        } else if (ifNumberIsAddedOnce != 1) {
+                            listOfThreeAndFourDigitLongNums.add(init);
+                            ifNumberIsAddedOnce = 1;
                         }
                     }
+                }
+                init = init + 1;
+                ifNumberIsAddedOnce = 0;
+                break;
+                case 3:
+                    length = 4;
+                    for (int j = 0; j < init / 10; j++) {
+                        if (init % listOfThreeAndFourDigitLongNums.get(j) == 0) {
+                            ifNumberIsAddedOnce = 0;
+                            j = init / 10;
+                            try {
+                                listOfThreeAndFourDigitLongNums.remove(listOfThreeAndFourDigitLongNums.indexOf(init));
+                            } catch (Exception e) {
+
+                            }
+                        } else if (ifNumberIsAddedOnce != 1) {
+                            listOfThreeAndFourDigitLongNums.add(init);
+                            ifNumberIsAddedOnce = 1;
+                        }
+                    }
+                    init = init + 1;
+                    ifNumberIsAddedOnce = 0;
+                    break;
+                case 4:
+                    length = 5;
+                    for (int j = 0; j < init / 100; j++) {
+                        if (init % listOfFiveAndSixDigitLongNums.get(j) == 0) {
+                            ifNumberIsAddedOnce = 0;
+                            j = init / 100;
+                            try {
+                                listOfFiveAndSixDigitLongNums.remove(listOfFiveAndSixDigitLongNums.indexOf(init));
+                            } catch (Exception e) {
+
+                            }
+                        } else if (ifNumberIsAddedOnce != 1) {
+                            listOfFiveAndSixDigitLongNums.add(init);
+                            ifNumberIsAddedOnce = 1;
+                        }
+                    }
+                    init = init + 1;
+                    ifNumberIsAddedOnce = 0;
+                    break;
+                case 5:
+                    length = 6;
+                    for (int j = 0; j < init / 100; j++) {
+                        if (init % listOfFiveAndSixDigitLongNums.get(j) == 0) {
+                            ifNumberIsAddedOnce = 0;
+                            j = init / 100;
+                            try {
+                                listOfFiveAndSixDigitLongNums.remove(listOfFiveAndSixDigitLongNums.indexOf(init));
+                            } catch (Exception e) {
+
+                            }
+                        } else if (ifNumberIsAddedOnce != 1) {
+                            listOfFiveAndSixDigitLongNums.add(init);
+                            ifNumberIsAddedOnce = 1;
+                        }
+                    }
+                    init = init + 1;
+                    ifNumberIsAddedOnce = 0;
+                    break;
+                default: {
+                }
+
+                    /*case 6:
+                    length=7;
+                    for (int j = 0; j < init/100; j++) {
+                        if(init%listOfFiveAndSixDigitLongNums.get(j)==0){
+                            ifNumberIsAddedOnce=0;
+                            j=init/100;
+                            try{
+                                listOfFiveAndSixDigitLongNums.remove(listOfFiveAndSixDigitLongNums.indexOf(init));
+                            }catch(Exception e){
+
+                            }
+                        }else
+                        if(ifNumberIsAddedOnce!=1){
+                            listOfFiveAndSixDigitLongNums.add(init);
+                            ifNumberIsAddedOnce=1;
+                        }
+                    }
+                    init=init+1;
+                    ifNumberIsAddedOnce=0;
+                    break;
+                    */
+                {//TESTING FOR PRIME NUMBER
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("enter the number");
+                    int inputInt = input.nextInt();
+                    int Length = (int) Math.log10(inputInt) + 1;
+                    if ((Length == 1) || (Length == 2)) {
+                        System.out.println(listOfTwoDigitLongNums.get(listOfTwoDigitLongNums.indexOf(inputInt)));
+                    }
+                    if ((Length == 3) || (Length == 4)) {
+                        System.out.println(listOfThreeAndFourDigitLongNums.get(listOfThreeAndFourDigitLongNums.indexOf(inputInt)));
+                    }
+                    if ((Length == 5) || (Length == 6)) {
+                        System.out.println(listOfFiveAndSixDigitLongNums.get(listOfFiveAndSixDigitLongNums.indexOf(inputInt)));
+                }
+            }
+
+
 
             }
+        }
+    }
+        public static void Test(){
+
         }
 
 
     }
-}
+
+
